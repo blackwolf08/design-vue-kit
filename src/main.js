9e42691053dca5d44d8cb641a2103ca54d51a978
Sunny Dhama <31814079+blackwolf08@users.noreply.github.com>
2019-04-09 02:45:00 +0530

    dist changes

--- src/main.js
+++ src/main.js
@@ -9,6 +9,9 @@ import 'bootstrap-italia';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-vue/dist/bootstrap-vue.min.css'
 import 'bootstrap-italia/dist/css/bootstrap-italia.min.css'
+import 'bootstrap-vue/dist/bootstrap-vue.min.js'
+import 'bootstrap-italia/dist/js/bootstrap-italia.min.js'
+import 'bootstrap/js/dist/button.js'
 
 //using BootstrapVue
 Vue.use(BootstrapVue)
