09fe8f552d7fa8e2505ccaa1e1888b9ce2606d2e
Sunny Dhama <31814079+blackwolf08@users.noreply.github.com>
2019-04-09 04:17:11 +0530

    Few more config changes

--- src/main.js
+++ src/main.js
@@ -1,20 +1,20 @@
 import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
 
-import App from './App.vue'
 
 //Importing necessary modules..
 import 'bootstrap';
 import 'bootstrap-vue';
 import 'bootstrap-italia';
+
+//using BootstrapVue
+Vue.use(BootstrapVue)
+
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-vue/dist/bootstrap-vue.min.css'
 import 'bootstrap-italia/dist/css/bootstrap-italia.min.css'
 
-
-
-//using BootstrapVue
-Vue.use(BootstrapVue)
+import App from './App.vue'
 
 Vue.config.productionTip = false
 
