fecd80ebe6bb0e7cdf242af70f87ebf62eedc46f
Sunny Dhama <31814079+blackwolf08@users.noreply.github.com>
2019-04-09 00:20:50 +0530

    Adding Storybook+Bootstrap Italia+other dependancies

---
+++ src/components/Button.vue
@@ -0,0 +1,27 @@
+<template>
+  <button class='btn' v-bind:class="[className, color ? (outline ? `btn-outline-${color}` : `btn-${color}`) : 'btn-primary' ]"  @click="onClick">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: {
+    msg: String,
+    color: String,
+    className: String,
+    outline: Boolean
+  },
+  methods: {
+    onClick() {
+      this.$emit('click')
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
+
--- src/main.js
+++ src/main.js
@@ -1,6 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+//Importing necessary modules..
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap';
+import 'bootstrap-vue';
+import 'bootstrap-italia';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-vue/dist/css/bootstrap-vue.min.css'
+import 'bootstrap-italia/dist/scss/bootstrap-italia.min.css'
+
+//using BootstrapVue
+Vue.use(BootstrapVue)
+
 Vue.config.productionTip = false
 
 new Vue({
