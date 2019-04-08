25a7a024b1ee34eb9f8aa82e8d3997471a5e079b
Sunny Dhama <31814079+blackwolf08@users.noreply.github.com>
2019-04-09 03:45:54 +0530

    Furthur Changes

--- src/main.js
+++ src/main.js
@@ -2,16 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 
 //Importing necessary modules..
-import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap';
 import 'bootstrap-vue';
 import 'bootstrap-italia';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-vue/dist/bootstrap-vue.min.css'
 import 'bootstrap-italia/dist/css/bootstrap-italia.min.css'
-import 'bootstrap-vue/dist/bootstrap-vue.min.js'
-import 'bootstrap-italia/dist/js/bootstrap-italia.min.js'
-import 'bootstrap/js/dist/button.js'
+
+import BootstrapVue from 'bootstrap-vue'
+
 
 //using BootstrapVue
 Vue.use(BootstrapVue)
