14d1c79ab3788cabdcd5a49d5276db02cde2e5f5
Sunny Dhama <31814079+blackwolf08@users.noreply.github.com>
2019-04-09 02:17:24 +0530

    Changes in path
    Changes in path

--- src/main.js
+++ src/main.js
@@ -7,8 +7,8 @@ import 'bootstrap';
 import 'bootstrap-vue';
 import 'bootstrap-italia';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap-vue/dist/css/bootstrap-vue.min.css'
-import 'bootstrap-italia/dist/scss/bootstrap-italia.min.css'
+import 'bootstrap-vue/dist/bootstrap-vue.min.css'
+import 'bootstrap-italia/dist/css/bootstrap-italia.min.css'
 
 //using BootstrapVue
 Vue.use(BootstrapVue)
@@ -17,4 +17,4 @@ Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
