64e4a59ee8c586dd79f59eb312fa67fff6dcd5aa
Sunny Dhama <31814079+blackwolf08@users.noreply.github.com>
2019-04-11 07:09:15 +0530

    beta-1

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,6 @@
 import { configure, addParameters } from '@storybook/vue';
+import { default as theme } from './theme.js';
+
 
 //importing bootstrap italia and bootstrap
 import 'bootstrap-italia/dist/css/bootstrap-italia.min.css';
@@ -10,7 +12,7 @@ addParameters({
   options: {
       name: 'Design Vue Kit',
       url: 'https://github.com/italia/design-vue-kit',
-      theme,
+      theme: theme,
   },
 });
 
