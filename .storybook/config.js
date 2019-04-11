3ae826ec59aa6277b2543de4b645a8d79f800b90
Sunny Dhama <31814079+blackwolf08@users.noreply.github.com>
2019-04-11 06:47:29 +0530

    Favicons and Config Changes

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,23 +1,23 @@
 import { configure, addParameters } from '@storybook/vue';
-import { default as theme } from './theme.js';
 
 //importing bootstrap italia and bootstrap
 import 'bootstrap-italia/dist/css/bootstrap-italia.min.css';
 import "../assets/docs.min.css";
 import "../assets/css/fonts.css";
 
-// automatically import all files ending in *.stories.js
-const req = require.context('../stories', true, /\.stories\.js$/);
-function loadStories() {
-  req.keys().forEach(filename => req(filename));
-}
+
 
 addParameters({
   options: {
       name: 'Design Vue Kit',
       url: 'https://github.com/italia/design-vue-kit',
-      theme: theme,
   },
 });
+
+// automatically import all files ending in *.stories.js
+const req = require.context('../stories', true, /\.stories\.js$/);
+function loadStories() {
+  req.keys().forEach(filename => req(filename));
+}
 
 configure(loadStories, module);
