9d2d842750a9a21e4e748672b3b764f00b06222a
Sunny Dhama <31814079+blackwolf08@users.noreply.github.com>
2019-04-11 06:08:31 +0530

    Assets and fonts import

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,11 +1,23 @@
-import { configure } from '@storybook/vue';
+import { configure, addParameters } from '@storybook/vue';
+import { default as theme } from './theme.js';
+
 //importing bootstrap italia and bootstrap
 import 'bootstrap-italia/dist/css/bootstrap-italia.min.css';
+import "../assets/docs.min.css";
+import "../assets/css/fonts.css";
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../stories', true, /\.stories\.js$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
+
+addParameters({
+  options: {
+      name: 'Design Vue Kit',
+      url: 'https://github.com/italia/design-vue-kit',
+      theme: theme,
+  },
+});
 
 configure(loadStories, module);
